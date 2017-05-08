01789ec7fc64e5269e084f93f76933028c74a4f1
vitkl <vitalii@Vitaliis-MacBook-Pro.local>
2017-05-08 10:49:50 +0100

    updated man, corrected error in kappa_score

--- R/kappa_score.R
+++ R/kappa_score.R
@@ -1,15 +1,12 @@
-##' GO term similarity - kappa score
-##' Function will calculate kappa score between two categories describing a set of 
-##' elements (GO terms, KEGG pathways, genes).
-##' Function takes a data.table with 2 columns describing to two categories to 
-##' which each element in rows can belong to. 
-##' Os and 1s in the table signify membership.
-##' defined as described in this article:
-##' https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2375021/figure/F2/
-##' relies on data.table for subsetting and vcd package for calculating kappa 
-##' score given 2x2 incidence matrix
-##' the function is faster than irr::kappa2(ratings = value, weight = "unweighted")
-##' the function is ~25 times slower than dist()
+##' similarity between categories - kappa score
+##' 
+##' @description Function will calculate kappa score between two categories describing a set of elements (GO terms, KEGG pathways, genes).
+##' @description Function takes a data.table with 2 columns describing to two categories to which each element in rows can belong to. Os and 1s in the table signify membership.
+##' @description the function is defined as described in this article: https://www.ncbi.nlm.nih.gov/pmc/articles/PMC2375021/figure/F2/
+##' @description relies on data.table for subsetting and vcd package for calculating kappa score given 2x2 incidence matrix
+##' 
+##' @description the function is faster than irr::kappa2(ratings = value, weight = "unweighted")
+##' @description the function is ~25 times slower than dist()
 ##'
 ##' @param value a data.table with 2 columns (two categories) and many rows (objects)
 ##' @importFrom vcd Kappa
@@ -19,7 +16,7 @@
 ##' @author Vitalii Kleshchevnikov
 kappa_score = function(value){
   if(!is.data.table(value)) stop("kappa_score: provided table may not be in the right format (wrong class: not data.table)")
-  if(ncol(mapping_table) > 2) stop("kappa_score: table has more than 2 category columns")
+  if(ncol(value) > 2) stop("kappa_score: table has more than 2 category columns")
       
   colnames(value) = c("x","y")
   table_ = value[,.N, by = .(x,y)]
@@ -32,16 +29,17 @@ kappa_score = function(value){
   return(kappa_score)
 }
 
-##' the categ_dist function to calculate categorical distance (Cohen's Kappa score) between multiple terms 
-##' the function is intended to measure distances between GO terms based on proteins they annotate
-##' more generally, the function can be used to measure categorical distances between any terms(categories) annotating objects
-##' objects should be provided as a first column of a data.table, terms should be provided as a second column
+##' calculate categorical distance (Cohen's Kappa score) between multiple categories
 ##' 
-##' Important: for correct evalutation mapping_table should inlude all 
-##' term-to-gene associations, terms_to_compare should specify which you want to compare
+##' @description The categ_dist function to calculate categorical distance (Cohen's Kappa score) between multiple terms (categories).
+##' @description The function is intended to measure distances between GO terms based on proteins they annotate.
+##' @description More generally, the function can be used to measure categorical distances between any terms(categories) annotating objects.
+##' @description Objects should be provided as a first column of a data.table, terms (categories) should be provided as a second column.
+##' 
+##' @description Important: for correct evalutation mapping_table should inlude all term-to-gene associations, terms_to_compare should specify which you want to compare
 ##' @param mapping_table two-column data.table which provides mapping from object to term 
 ##' @param terms_to_compare a character vector specifying which terms to compare
-##' @param ignore_limit logical, ignore the limit set on the number of terms
+##' @param ignore_limit logical, ignore the limit set on the number of terms to compare (1000)
 ##' @import data.table
 ##' @importFrom caTools combs
 ##' @export
